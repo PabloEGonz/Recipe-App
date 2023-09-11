126baab4b768f84b6673cfdd1f76f8d660d879fc
Pablo <ezekielgonzalezr@gmail.com>
2023-09-10 21:16:13 -0600

    Set up model associations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+  has_many :inventory_foods
+  has_many :inventories, through: :inventory_foods
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }

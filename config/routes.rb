8a8d4e216ce7fac5dcbc12e68b995ecf90f7a49e
Pablo <ezekielgonzalezr@gmail.com>
2023-09-10 21:47:52 -0600

    Add inventory routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/user_account', to: 'users#index'
   resources :foods, only: [:index, :new, :create, :show, :destroy ]
+  resources :inventories, only: [:index, :new, :create, :show, :destroy ]
   resources :recipes, only: [:index, :update, :new, :create, :show, :destroy ] do
   resources :recipe_foods, only: [:update, :new, :create, :destroy ]
   end
